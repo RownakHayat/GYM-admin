c483e9393d2287237068fc7d45ad731169f7d673
unknown <rownakhayat@gmail.com>
2023-10-11 18:18:27 +0600

    first commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,32 +1,36 @@
 import React from 'react'
-import {BsSearch} from 'react-icons/bs'
-import {AiOutlineAppstore, AiOutlineExpand,AiOutlineUser, AiOutlineSetting} from 'react-icons/ai'
-import {IoNotificationsOutline} from 'react-icons/io5'
+import {FaSearch, FaExpand} from 'react-icons/fa'
+import {GrAppsRounded} from 'react-icons/gr'
+import {MdNotificationsNone} from 'react-icons/md'
+import {BiUser} from 'react-icons/bi'
+import {LuSettings} from 'react-icons/lu'
+import '../../styles/global.scss'
 
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between bg-[#0f020b] text-[#f6f7ed] w-100% p">
+    <div className="flex justify-between bg-[#0f020b] text-[#f6f7ed] w-100%">
       <div className="log">
         <img src=".../../../../../public/assets/image/top-header-logo.png" alt="" className='bg-[#868524] w-20 rounded-full shadow p-1' />
         <span></span>
       </div>
-      <div className="icons flex gap-2 py-6">
-        <BsSearch />
-        <AiOutlineAppstore />
-        <AiOutlineExpand />
+      <div className="icons flex gap-6 py-6 mr-8 font-bold">
+        <FaSearch />
+        <GrAppsRounded />
+        <FaExpand />
         <div className="notice">
-        <IoNotificationsOutline className='absolute' />
-        <span className='bg-red-500 rounded-full text-xs p-1 relative'>1</span>
+        <MdNotificationsNone />
+        <span className='bg-red-500 rounded-full w-4 h-2 p-1 relative'>1</span>
         </div>
         <div className="user">
           {/* <img src="" alt="" className="w-8 h-8" /> */}
-          <AiOutlineUser />
+          <BiUser />
           <p>User</p>
         </div>
         
-        <AiOutlineSetting />
+        <LuSettings />
       </div>
+      
     </div>
   )
 }
