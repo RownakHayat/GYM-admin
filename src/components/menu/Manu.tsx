5a39e1147869b6844feceb507e9a378dad8c9bdb
unknown <rownakhayat@gmail.com>
2023-10-12 13:55:43 +0600

    push

--- src/components/menu/Manu.tsx
+++ src/components/menu/Manu.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+import { menu } from '../../../data';
 
 const Manu = () => {
   return (
-    <div>Manu</div>
+    <div className='menu'>
+      {menu.map(item => (
+        <div className="items" key={item.id}>
+          <span className='Dashboard'>{item.title}</span>
+          {item.listItems.map(listItem => (
+
+            <Link to="/" className='list-item' key={listItem.id} >
+              <img src={listItem.icon} alt="" className='w-10' />
+              <span className='listItemTitle'>{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+
+    </div>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=''>Home</div>
+    <div className='home gap-4 grid grid-cols-4'>
+      <div className="box box1 py-8 px-20">box1</div>
+      <div className="box box2 py-8 px-20">box1</div>
+      <div className="box box3 py-8 px-20">box1</div>
+      <div className="box box4 py-8 px-20">box1</div>
+      <div className="box box5 py-8 px-20">box1</div>
+      <div className="box box6 py-8 px-20">box1</div>
+      <div className="box box7 py-8 px-20">box1</div>
+      <div className="box box8 py-8 px-20">box1</div>
+      <div className="box box9 py-8 px-20">box1</div>
+      <div className="box box10 py-8 px-20">box1</div>
+      <div className="box box11 py-8 px-20">box1</div>
+    </div>
   )
 }
 
